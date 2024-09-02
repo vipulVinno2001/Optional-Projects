1a485b5b86b1bf6254e7d5d2838a9d6617166e42
Vipul <vinno.vinno@gmail.com>
2024-09-02 18:19:43 +0530

    Updated the Project

--- src/Components/NewExp/ExpenseForm.jsx
+++ src/Components/NewExp/ExpenseForm.jsx
@@ -1,9 +1,52 @@
+import { useState } from "react";
+
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
+  const titleChangeHandler = (e) => {
+    setEnteredTitle(e.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //     enteredTitle:e.target.value,
+
+    // })
+
+    // setUserInput((prevState) => {
+    //   return {
+    //     ...prevState,
+    //     enteredTitle: e.target.value,
+    //   };
+    // });
+  };
+
+  const amountChnageHandler = (e) => {
+    setEnteredAmount(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: e.target.value,
+    // });
+  };
+
+  const dateChangeHandler = (e) => {
+    setEnteredDate(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: e.target.value,
+    // });
+  };
   return (
     <div className="flex  justify-center p-6 ">
-      <form className="bg-blue-900 rounded-2xl p-10 flex flex-col   gap-4" action="">
+      <form
+        className="bg-blue-900 rounded-2xl p-10 flex flex-col   gap-4"
+        action=""
+      >
         <div className="flex">
-          {" "}
           <div className="">
             <label className="p-2  text-white font-semibold" htmlFor="">
               Title:
@@ -11,6 +54,7 @@ const ExpenseForm = () => {
             <input
               className="rounded-lg p-1 text-black font-semibold bg-slate-400 border-1 border-black"
               type="text"
+              onChange={titleChangeHandler}
             />
           </div>
           <div>
@@ -22,6 +66,7 @@ const ExpenseForm = () => {
               type="number"
               min="0.01"
               step="0.01"
+              onChange={amountChnageHandler}
             />
           </div>
           <div>
@@ -33,12 +78,16 @@ const ExpenseForm = () => {
               type="date"
               min="2019-01-01"
               max="2022-12-31"
+              onChange={dateChangeHandler}
             />
           </div>
         </div>
 
         <div className="flex justify-center">
-          <button className="p-2 border-2 border-white  bg-green-600 rounded-2xl " type="submit">
+          <button
+            className="p-2 border-2 border-white  bg-green-600 rounded-2xl "
+            type="submit"
+          >
             Add Expense
           </button>
         </div>
